5f3f6816689b46d8a9039f079c06c6b1d5d057a3
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-28 15:58:17 +0100

    cache warning fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,23 @@ import Header from "./components/Header";
 import Brands from "./components/Brands";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        brands: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:8080/graphql",
-  cache: new InMemoryCache(),
+  cache: cache,
 });
 
 function App() {
