e239001c92eb854010bfee4cb7ba84ba1b3e2f7a
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-28 15:59:23 +0100

    delete brands function

--- client/src/components/BrandsRow.jsx
+++ client/src/components/BrandsRow.jsx
@@ -1,6 +1,20 @@
 import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_BRAND } from "../mutations/brandMutations";
+import { GET_BRANDS } from "../queries/brandQueries";
 
 export default function BrandsRow({ brand }) {
+  const [deleteBrand] = useMutation(DELETE_BRAND, {
+    variables: { id: brand.id },
+    // refetchQueries: [{ query: GET_BRANDS }],
+    update(cache, { data: { deleteBrand } }) {
+      const { brands } = cache.readQuery({ query: GET_BRANDS });
+      cache.writeQuery({
+        query: GET_BRANDS,
+        data: { brands: brands.filter((b) => b.id !== deleteBrand.id) },
+      });
+    },
+  });
   return (
     <tr>
       <td>{brand.IPR}</td>
@@ -12,7 +26,7 @@ export default function BrandsRow({ brand }) {
       <td>{brand.Nice_classification}</td>
       <td>{brand.Owner}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={deleteBrand}>
           <FaTrash />
         </button>
       </td>
